2f4eb3c4ed88d8d2efcad67ef744c69617b8b395
n0bxii <kimnoura8@gmail.com>
2020-06-13 23:20:28 +0300

    Homwork-1 is done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.5
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,19 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = " fouz"
+var lastName1 = "alajmi"
+var email1 = "fouza12@gmail.com"
+var phoneNumber1 = "97862328"
+var age1 = 15
+var country1 = "kuwait"
+var area1 = " qasr"
+var password1 = "1000080"
+var isKuwaiti1 = true
 
 //Person 2
-
-
+var firstname2 = " lulu "
+var lastName2 = "alhabashi"
+var email2 = "lulu@gmail.com"
+var phoneNumber2 = "99671521"
+var age2 = 14
+var country2 = "kuwait"
+var area2 = " qasr"
+var password2 = "1008080"
+var isKuwaiti2 = true
 
 
 
@@ -60,8 +67,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
+var firstName3 = " noor"
+var lastName3 = "alhabashi"
+var email3 = "kimnoura8@gmail.com"
+var phoneNumber3 = "94979567"
+var age3 = 15
+var country3 = "kuwait"
+var area3 = " qasr"
+var password3 = "1088080"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +96,88 @@ var isKuwaiti = false
 
  
  */
+
+firstName1 == firstname2
+firstName1 == firstName3
+firstname2 == firstName3
+firstName1 != firstname2
+firstName1 != firstName3
+firstname2 != firstName3
+
+lastName1 == lastName2
+lastName1 == lastName3
+lastName2 == lastName3
+lastName1 != lastName2
+lastName1 != lastName3
+lastName2 != lastName3
+
+email1 == email2
+email1 == email3
+email2 == email3
+email1 != email2
+email1 != email3
+email2 != email3
+
+age1 == age2
+age1 == age3
+age2 == age3
+age1 != age2
+age1 != age3
+age2 != age3
+age1 > age2
+age1 > age3
+age2 > age3
+age1 >= age2
+age1 >= age3
+age2 >= age3
+age1 < age2
+age1 < age3
+age2 < age3
+age1 <= age2
+age1 <= age3
+age2 <= age3
+
+country1 == country2
+country1 == country3
+country2 == country3
+country1 != country2
+country1 != country3
+country2 != country3
+
+area1 == area2
+area1 == area3
+area2 == area3
+area1 != area2
+area1 != area3
+area2 != area3
+
+password1 == password2
+password1 == password3
+password2 == password2
+password1 != password2
+password1 != password3
+password2 != password3
+
+isKuwaiti1 == isKuwaiti2
+isKuwaiti1 == isKuwaiti3
+isKuwaiti2 == isKuwaiti3
+isKuwaiti1 != isKuwaiti2
+isKuwaiti1 != isKuwaiti3
+isKuwaiti2 != isKuwaiti3
+
+18 < age1
+18 < age2
+18 < age3
+
+18 > age1
+18 > age2
+18 > age3
+
+age1 == 17
+age2 == 17
+age3 == 17
+age1 != 17
+age2 != 17
+age3 != 17
+
+
